a9ed315a3ddba8e1a2a73d1890217dead6594909
John Cintron <squash@shadowfall.me>
2017-05-25 20:43:45 -0400

    Change from batch files to Powershell for Logon/Logoff scripts
    Switch to powershell scripts instead of batch files.
    
    This removes the dependency on curl and instead uses Powershell's Invoke-WebRequest.

---
+++ Lab Usage Windows Script/Logon/LabUsage-LogOn.ps1
@@ -0,0 +1,15 @@
+﻿# Set server URL for database updates
+
+$server_url='http://server/labusage/labavailwriter.php'
+
+# Get hostname from workstation
+
+$hostname=hostname
+
+# Set parameters
+
+$params=@{machine=$hostname;mstatus='1'}
+
+# Run Invoke-WebRequest to write availability status to server
+
+Invoke-WebRequest -UseBasicParsing -Uri $server_url -Method POST -Body $params
